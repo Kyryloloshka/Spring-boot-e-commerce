3db329a36e94a3dffc0166e700a8f0f7ab1cd6f0
Kyryloloshka <kirill.contrmptuous@gmail.com>
2026-01-15 17:49:07 +0200

    feat(cosmo): init project

---
+++ src/main/java/com/example/web_market_cosmo_cats/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_market_cosmo_cats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
